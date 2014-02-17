6b66344a28ecced41cb443c48a8d7eebf5c70361
ttyS15 <ttyS15@yandex.ru>
2014-02-17 16:25:25 +0400

    # up to 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-celery-rpc',
-    version='0.3',
+    version='0.4',
     packages=['celery_rpc'],
     url='https://github.com/ttyS15/django-celery-rpc',
     license='Public',
