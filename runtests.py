c6d1dd1f212e56341c52cca2e492ee4e264ce51d
Anatoly Larin <anatoliy@larin.in>
2014-03-06 17:12:18 +0400

    Configuration Travis CI & test runner

---
+++ runtests.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import sys
+from optparse import OptionParser
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure(
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'django_celery_rpc',
+                'USER': '',
+                'PASSWORD': '',
+                'HOST': '',
+                'PORT': '',
+            }
+        },
+
+        INSTALLED_APPS=[
+            'django.contrib.contenttypes',
+            'celery_rpc',
+            'celery_rpc.tests',
+        ],
+        ROOT_URLCONF='',
+        DEBUG=False,
+    )
+
+
+from django_nose import NoseTestSuiteRunner
+
+
+def runtests(*test_args, **kwargs):
+    if 'south' in settings.INSTALLED_APPS:
+        from south.management.commands import patch_for_test_db_setup
+        patch_for_test_db_setup()
+
+    if not test_args:
+        test_args = ['celery_rpc']
+
+    test_runner = NoseTestSuiteRunner(**kwargs)
+
+    failures = test_runner.run_tests(test_args)
+    sys.exit(failures)
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option('--verbosity', dest='verbosity', action='store',
+                      default=1, type=int)
+    parser.add_options(NoseTestSuiteRunner.options)
+    (options, args) = parser.parse_args()
+
+    runtests(*args, **options.__dict__)
