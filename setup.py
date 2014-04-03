407809666d14a594ab7e4ca06f2154de840eccce
ttyS15 <ttyS15@yandex.ru>
2014-04-03 13:22:43 +0400

    Up to 0.9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-celery-rpc',
-    version='0.7',
+    version='0.9',
     packages=find_packages(),
     url='https://github.com/ttyS15/django-celery-rpc',
     license='Public',
