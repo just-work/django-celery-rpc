e807c04b4d87909624683e796924a590f68b8fc4
Sergey Tikhonov <zimbler@gmail.com>
2014-08-12 13:41:27 +0400

    try-catch for django missing, fix requires

--- celery_rpc/encoders.py
+++ celery_rpc/encoders.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals
 
-from django.utils.functional import Promise
-from django.utils.encoding import force_unicode
+try:
+    from django.utils.functional import Promise
+    from django.utils.encoding import force_unicode
+    has_django = True
+except ImportError:
+    has_django = False
 import datetime
 import decimal
 import json
@@ -16,12 +20,10 @@ class XJSONEncoder(json.JSONEncoder):
     https://github.com/tomchristie/django-rest-framework/blob/master/rest_framework/utils/encoders.py
 
     """
-    def default(self, o):
+    def _default(self, o):
         # For Date Time string spec, see ECMA 262
         # http://ecma-international.org/ecma-262/5.1/#sec-15.9.1.15
-        if isinstance(o, Promise):
-            return force_unicode(o)
-        elif isinstance(o, datetime.datetime):
+        if isinstance(o, datetime.datetime):
             r = o.isoformat()
             if o.microsecond:
                 r = r[:23] + r[26:]
@@ -44,3 +46,13 @@ class XJSONEncoder(json.JSONEncoder):
         elif hasattr(o, '__iter__'):
             return [i for i in o]
         return super(XJSONEncoder, self).default(o)
+
+    if has_django:
+        # Handling django-specific classes only if django package is installed
+        def default(self, o):
+            if isinstance(o, Promise):
+                return force_unicode(o)
+            else:
+                return self._default(o)
+    else:
+        default = _default
