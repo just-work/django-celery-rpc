393f900c3478eb154c9079ba88548eb5d666cec2
Sergey Tikhonov <zimbler@gmail.com>
2014-08-12 13:52:41 +0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     ],
     tests_require=[
         'django >=1.3, <1.7',
-        'djangorestframework >= 2.3, <2.4'
+        'djangorestframework >= 2.3, <2.4',
         'django-nose>=0.1.3',
         'django-autofixture==0.3.2',
         'mock==1.0.1',
