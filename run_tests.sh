aa57347586445a8a17e56c7ddbfa63ffb015b130
Sergey Tikhonov <zimbler@gmail.com>
2014-10-06 14:46:55 +0400

    fix detecting Django version and running django.setup() for django==1.7

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,11 @@
 
 PYTHON_VERSION=$(python --version 2>&1)
 
+if [[ -z "$DJANGO" ]]
+then
+  DJANGO=`pip freeze|grep Django==`
+fi
+
 if [[ "$PYTHON_VERSION" > "Python 3" ]]
 then
   if [[ "$DJANGO" < "1.5" ]]
