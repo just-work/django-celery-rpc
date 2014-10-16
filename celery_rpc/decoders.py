e982c06f9fabe46cfb6618d899793aacab197045
Timur Kojevnikov <voron3x@gmail.com>
2014-10-16 17:50:43 +0400

    python 3 support

--- celery_rpc/decoders.py
+++ celery_rpc/decoders.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-from six import string_types
+import six
 import jsonpickle
 import re
 
@@ -9,12 +9,13 @@ def x_json_decoder_object_hook(val):
     """
     Iter through dict for additional conversion
     """
-    for k, v in val.iteritems():
+
+    for k, v in six.iteritems(val):
         if isinstance(v, dict):
             x_json_decoder_object_hook(v)
         else:
             # django Q object decode
             reg_ex = '\"py/object\": \"django.db.models.query_utils.Q\"'
-            if isinstance(v, string_types) and re.search(reg_ex, v):
+            if isinstance(v, six.string_types) and re.search(reg_ex, v):
                 val[k] = jsonpickle.decode(v)
     return val
