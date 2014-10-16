db3665a3cc3fbf063d8333865ecfc7f56a73ed32
Timur Kojevnikov <voron3x@gmail.com>
2014-10-16 13:13:33 +0400

    jsonpickle support

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'celery >=3.1.5, <3.2.0',
         'jsonpickle>=0.8.0',
         'six',
+        'jsonpickle>=0.8.0',
         # celery_rpc server still needs django and djangorestframework packages,
         # but they are unnecessary for client
 
