7a8b7ae918acbfeeaf9a1c567b82347c3fbff5c6
ttyS15 <ttyS15@yandex.ru>
2014-10-27 15:23:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ else:
 
 setup(
     name='django-celery-rpc',
-    version='0.17',
+    version='0.18',
     packages=find_packages(),
     url='https://github.com/ttyS15/django-celery-rpc',
     license='Public',
