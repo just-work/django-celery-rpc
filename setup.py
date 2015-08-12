83753bc8b81bd5347112aea59a69fdebe185db48
Alexey Manaev <ttyS15@users.noreply.github.com>
2015-08-12 13:08:41 +0300

    Up to 0.23-rc1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ else:
 
 setup(
     name='django-celery-rpc',
-    version='0.22',
+    version='0.23-rc1',
     packages=find_packages(),
     url='https://github.com/ttyS15/django-celery-rpc',
     license='Public',
