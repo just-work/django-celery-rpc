a23aa36a49bebddbbdbbe3e5511dc5d26e6741ea
Yaroslav Shukharev <krkmetal@gmail.com>
2019-10-01 00:26:10 +1000

    removed unused import

--- celery_rpc/client.py
+++ celery_rpc/client.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 import os
 import socket
 
-from celery.exceptions import TimeoutError, TaskRevokedError
+from celery.exceptions import TimeoutError
 from celery.utils import nodename
 
 from . import utils
