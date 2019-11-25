8c78c0e70c3ae91a0f1e68b98f808ef8da955258
Yaroslav Shukharev <krkmetal@gmail.com>
2019-11-26 02:59:49 +1000

    review fixes

--- celery_rpc/tests/utils.py
+++ celery_rpc/tests/utils.py
@@ -69,29 +69,3 @@ class unpack_exception(object):
         inner = utils.unpack_exception(exc_val, True)
         exc_val = inner or exc_val
         raise exc_val
-
-
-def create_m2m(field_name, field_factory=None):
-    """ Вспомогательная функция для создания Many-To-Many полей
-    для PostGeneration декрарации factory_boy.
-
-    Сделано на основе документации по factory_boy:
-    https://factoryboy.readthedocs.io/en/latest/recipes.html#simple-many-to-many-relationship
-
-    Если указан field_factory, то поле поле заполняется единственным объектом,
-    созданным указанной фабрикой.
-
-    :param field_name: Имя поля, в которое будут добавляться объекты.
-    :param field_factory: Фабрика для создания одного объекта.
-    """
-    def basic_m2m(obj, create, extracted, **kwargs):
-        if not create:
-            return
-
-        if field_factory is not None:
-            getattr(obj, field_name).add(field_factory.create())
-        elif extracted:
-            for item in extracted:
-                getattr(obj, field_name).add(item)
-
-    return basic_m2m
