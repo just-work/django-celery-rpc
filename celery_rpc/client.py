f10130a2137448cf49c0c5b3c9031ac475a8e4d2
klem4 <a.klimenko@just-work.org>
2020-08-13 10:10:24 +0300

    vpcmn-43 **get_opts -> **options in Client.get_result

--- celery_rpc/client.py
+++ celery_rpc/client.py
@@ -277,7 +277,7 @@ class Client(object):
                                       high_priority=high_priority, **options)
         return self.send_request(signature, nowait, timeout, retries)
 
-    def get_result(self, async_result, timeout=None, **kwargs):
+    def get_result(self, async_result, timeout=None, **options):
         """ Collect results from delayed result object
 
         :param async_result: Celery AsyncResult object
@@ -289,10 +289,9 @@ class Client(object):
 
         """
         timeout = timeout or GET_RESULT_TIMEOUT
-        get_opts = kwargs.get('get_opts', {})
 
         try:
-            return async_result.get(timeout=timeout, **get_opts)
+            return async_result.get(timeout=timeout, **options)
         except TimeoutError:
             raise self.TimeoutError('Timeout exceeded while waiting for results')
         except RestFrameworkError:
